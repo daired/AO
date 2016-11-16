52210283dd8c052bd12fb898b81b7432bafb6e5e
Alexander Becker <Alexander Becker>
2016-11-16 15:48:10 +0100

    Commit von insertionSort

--- src/main/java/hk/htw/ao/function/InsertionSort.java
+++ src/main/java/hk/htw/ao/function/InsertionSort.java
@@ -8,7 +8,7 @@ import hk.htw.ao.util.OptimizedRandom;
 public class InsertionSort extends FunctionThread {
 
 	protected final OptimizedRandom RANDOM = OptimizedRandom.getInstance();
-	private BigInteger[] res;
+	private int[] res;
 
 	public InsertionSort(String[] values) {
 		super(values);
@@ -18,15 +18,16 @@ public class InsertionSort extends FunctionThread {
 			// Input values, call function(s) and set result
 			protected Object call() throws Exception {
 				
-				BigInteger[] array = RANDOM.generateRandomUnsortedList(
-						new Integer(parameter[1]), new Integer(parameter[0]));
-				
-				return res = mergeSort(array);			
+//				BigInteger[] array = RANDOM.generateRandomUnsortedList(
+//						new Integer(parameter[1]), new Integer(parameter[0]));
+//				
+//				return res = mergeSort(array);		
+				return null;
 		    }
 		};
 	}
 
-	public BigInteger[] getRes() {
+	public int[] getRes() {
 		return res;
 	}
 
@@ -38,57 +39,28 @@ public class InsertionSort extends FunctionThread {
 	 * 
 	 * bigInt[] --> bigInt[]
 	 */
-	public static BigInteger[] mergeSort(BigInteger[] array){
-		
-		if (array.length > 1) {
-			int q = array.length / 2;
-			BigInteger[] l = new BigInteger[q];
-			for(int i = 0; i<= l.length-1; i++)
-				l[i] = array[i];
-			
-			BigInteger[] r = new BigInteger[array.length-q];
-			for(int i = q; i <= array.length-1; i++)
-				r[i - q] = array[i];
-			
-			mergeSort(l);
-			mergeSort(r);
-	
-			array = merge(l,r);
-		}
-		return array;
+	public  static int[] insertionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (a[j] > a[j-1]) {
+                    break;
+                }
+                swap(a, j-1, j);
+            }
+        }
+        return a;
 	}
 	
-	// Helper 
-	private static BigInteger[] merge(BigInteger[] l, BigInteger[] r){
-		BigInteger[] array = new BigInteger[l.length + r.length];
-		int iL = 0;
-		int iR = 0;
-		int iRes = 0;
-		
-		while (iL < l.length && iR < r.length) {
-			if (l[iL].compareTo(r[iR]) < 0) {
-				array[iRes] = l[iL];
-				iL += 1;
-			} else {
-				array[iRes] = r[iR];
-				iR++;
-			}
-			iRes++;
-		}
-
-		while (iL < l.length) {
-			array[iRes] = l[iL];
-			iL++;
-			iRes++;
-		}
-
-		while (iR < r.length) {
-			array[iRes] = r[iR];
-			iR++;
-			iRes++;
-		}
-		
-		return array;
-	}
+	
+	private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+	
+	 
+    
+	
 
 }
